57c826872aa49add69142a0036049a73dc419f5b
Andy <Andy30612@outlook.com>
2018-06-02 12:23:52 +0800

    association

--- src/main/java/org/andylabs/demo/controller/OrginationController.java
+++ src/main/java/org/andylabs/demo/controller/OrginationController.java
@@ -1,7 +1,6 @@
 package org.andylabs.demo.controller;
 
-import org.andylabs.demo.domain.Enterprise;
-import org.andylabs.demo.mappers.EnterpriseMapper;
+import org.andylabs.demo.mappers.EnterpriseRepository;
 import org.andylabs.demo.mappers.UserAnnotationMapper;
 import org.andylabs.demo.mappers.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,23 +11,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "root")
-public class OrginationController
-{
+public class OrginationController {
     @Autowired
     private UserMapper userMapper;
 
-    @Autowired private UserAnnotationMapper userAnnotationMapper;
+    @Autowired
+    private UserAnnotationMapper userAnnotationMapper;
 
     @Autowired
-    private EnterpriseMapper enterpriseMapper;
+    private EnterpriseRepository enterpriseRepository;
 
     @RequestMapping(value = "test", method = RequestMethod.GET)
     public String retrieve(@RequestParam(value = "parkSet", required = false) String parkSet,
-                                                                               @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
-                                                                               @RequestParam(value = "size", required = false, defaultValue = "10") Integer size) {
+                           @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
+                           @RequestParam(value = "size", required = false, defaultValue = "10") Integer size) {
         var list = userMapper.findAllUsers();
         var list2 = userAnnotationMapper.findAllUsers();
-        var list3 = enterpriseMapper.findAllEnterprise();
+        var list3 = enterpriseRepository.findAllEnterprise();
+        var enterpriseId = "2";
+        var code = "002";
+        var entity = enterpriseRepository.findEnterprise( code);
         return "123";
     }
 }
--- src/main/java/org/andylabs/demo/mappers/EnterpriseRepository.java
+++ src/main/java/org/andylabs/demo/mappers/EnterpriseRepository.java
@@ -1,11 +1,14 @@
 package org.andylabs.demo.mappers;
 
 import org.andylabs.demo.domain.Enterprise;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface EnterpriseMapper {
+public interface EnterpriseRepository {
     List<Enterprise> findAllEnterprise();
+
+    List<Enterprise> findEnterprise(@Param("mycode") String code);
 }
